17d0ca17f8071e612ca54cea8780240271d19634
Rudresh Pandey <rudreshpandey2002@gmail.com>
2023-07-07 23:24:14 +0530

    update div element and Boxdesign UI fixed

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function Game() {
-  const Game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const [currPlayer, setCurrPlayer] = useState(1);
+  // const [clickedBox, setClickedBox] = useState<number[]>([]);
+
+  const clickedBox = [];
+  const Game = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  function fillBox(e) {
+    clickedBox.push(e.target.value);
+    if (currPlayer === 1) {
+      e.target.innerHTML = `X`;
+      e.target.style = "color: rgb(20 184 166)";
+      setCurrPlayer(2);
+    } else if (currPlayer === 2) {
+      e.target.innerHTML = "O";
+      e.target.style = "color: rgb(234 179 8)";
+      setCurrPlayer(1);
+    }
+  }
 
   return (
     <div className="text-center flex h-screen text-white font-montserrat">
@@ -18,11 +37,12 @@ function Game() {
               Player 2 : <span className=" text-xl text-yellow-500">O</span>
             </p>
           </div>
-          <div className=" flex flex-col gap-8">
+          {/* <div className=" flex flex-col gap-8">
             <div className=" flex justify-between">
               <div
                 className=" box-design"
                 style={{ width: "100px", height: "100px" }}
+                onClick={fillBox}
               ></div>
               <div
                 className=" box-design"
@@ -61,6 +81,24 @@ function Game() {
                 style={{ width: "100px", height: "100px" }}
               ></div>
             </div>
+          </div> */}
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gap: "2rem",
+            }}
+          >
+            {Game.map((box) => {
+              return (
+                <button
+                  key={box}
+                  value={box}
+                  className=" box-design font-bold text-4xl"
+                  onClick={fillBox}
+                ></button>
+              );
+            })}
           </div>
         </div>
       </div>
